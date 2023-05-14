20d8d6d5da8106834eec16042501483c33a325e8
ronevych <dmytro874@gmail.com>
2023-05-14 20:30:45 +0300

    updated

--- Zoo.py
+++ Zoo.py
@@ -1,7 +1,9 @@
 class Zoo:
     
-    def __init__(self, name, location, area, capacity):
-        self.       name = name
+    instance = None
+    
+    def __init__(self, name = "", location = "", area = 0, capacity = 0):
+        self.name = name
         self.location = location
         self.area = area
         self.capacity = capacity
@@ -14,4 +16,18 @@ class Zoo:
         
     def add_new_region(self, new_area):
         self.area += new_area
-
+        
+    def __str__(self):
+        return (f"Zoo(name : {self.name}, location : {self.location}, area : "
+                f"{self.area}, capacity : {self.capacity})") 
+    
+    @staticmethod
+    def get_instance():
+        Zoo.instance = Zoo()
+        return Zoo.instance
+    
+zoo1 = Zoo("Lvivs`kyy", "Lviv", 1000, 500)
+zoo2 = Zoo()
+zoo_list = [zoo1, zoo2, Zoo.get_instance()]
+for i in zoo_list:
+    print(i)
